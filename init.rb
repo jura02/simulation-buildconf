4934a4e9ff4a25d7d52c41ebd52d21bd08b44598
Raul.Dominguez <raul.dominguez@dfki.de>
2018-07-06 10:01:15 +0200

    Add dfki git server configuration

--- init.rb
+++ init.rb
@@ -37,6 +37,8 @@ Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 Autoproj.gitorious_server_configuration('SPACEGIT', 'git.hb.dfki.de',:fallback_to_http => false)
 Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 
+Autoproj.gitorious_server_configuration('DFKIGIT', 'git.hb.dfki.de', :fallback_to_http => false, default: 'ssh,ssh', disabled_methods: 'http,git')
+
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
 
 if(Autobuild.respond_to?("displayed_error_line_count"))
